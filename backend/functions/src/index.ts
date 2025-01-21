14df32c87603aa2a90101fe71cf78d2f804d3146
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2025-01-20 19:35:02 -0500

    Exported functions for functions emulator depl

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,9 +2,15 @@ import './config';
 /*  Tell Firebase which functions to deploy and make available to your application. */
 import { addPaymentMethod } from './users/restful/addPaymentMethod';
 import { onUserCreated } from './users/reactive/onUserCreated';
+import { updateStats } from './users/reactive/updateStats';
+import { stopCodingSession } from './users/restful/stopCodingSession';
+import { trackCodingSession } from './users/restful/trackCodingSession';
 
 
 export {
     addPaymentMethod,
-    onUserCreated
+    onUserCreated,
+    updateStats,
+    stopCodingSession,
+    trackCodingSession
 };
